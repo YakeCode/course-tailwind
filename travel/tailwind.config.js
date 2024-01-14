ab985d62ee0ea823acd374e4c286fb5451fdb660
YakeCode <y.gaviriaescobar@gmail.com>
2024-01-13 23:12:25 -0500

    configuracion para empezar el proyecto realizada

---
+++ travel/tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode:false,
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+
+      backgroundImage: {
+        'sanFrancisco': "url('../img/sanFrancisco.jpg')",
+			  'sanFranciscoDesktop': "url('../img/sanFranciscoDesktop.jpg')",
+			  'yosemite': "url('../img/yosemite.jpg')",
+			  'LA': "url('../img/LA.jpg')",
+			  'seattle': "url('../img/seattle.jpg')",
+			  'new_york': "url('../img/new_york.jpg')",
+			  'norway': "url('../img/norway.jpg')",
+			  'sydney': "url('../img/sydney.jpg')",
+			  'miami': "url('../img/miami.jpg')",
+			  'switzerland': "url('../img/switzerland.jpg')",
+			  'bali': "url('../img/bali.jpg')",
+			  'norway': "url('../img/norway.jpg')",
+			  'chicago': "url('../img/chicago.jpg')",
+			  'europe': "url('../img/europe.jpg')",
+			  'iceland': "url('../img/iceland.jpg')",
+      },
+
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      'primary': '#cc2d4a',
+      'secondary': '#8fa206',
+      'terciary': '#61aec9',
+    
+    }),
+    textColor:{
+      'primary': '#cc2d4a',
+      'secondary': '#8fa206',
+      'terciary': '#61aec9',
+    },
+
+    fontFamily:{
+      Monserrat:['Monserrat', 'sans-serif']
+    }
+      
+    },
+  },
+  plugins: [],
+}
